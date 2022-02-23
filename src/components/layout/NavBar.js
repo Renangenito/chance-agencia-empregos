2fdf61ef2bbb2222556244d805dbf38192bfea5c
Renan Bezerra de Lima <77756047+Renangenito@users.noreply.github.com>
2022-02-23 14:35:15 -0300

    Finalizando a NavBar!

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,31 +4,33 @@ import { Link } from 'react-router-dom';
 import {MdWork, MdHome, MdSearch, MdPerson} from 'react-icons/md'
 function NavBar(){
     return(
+       
         <div className={styles.navBar}>
             <div className={styles.navBar_logo}>
                 Chance
             </div>
-            <div className={styles.navBar_link}>
-                <Link to="/">
-                <MdWork /> Inicio
+            <div className={styles.navBar_links}>
+                <Link className={styles.navBar_link} to="/">
+                <MdHome /> <p>Inicio</p>
                 </Link>
             </div>
-            <div className={styles.navBar_link}>
-                <Link to="/candidaturas">
-                <MdHome /> Candidaturas
+            <div className={styles.navBar_links}>
+                <Link className={styles.navBar_link} to="/candidaturas">
+                <MdWork /> <p>Candidaturas</p>
                 </Link>
             </div>
-            <div className={styles.navBar_link}>
-                <Link to="/vagas">
-                <MdSearch /> Vagas
+            <div className={styles.navBar_links}>
+                <Link className={styles.navBar_link} to="/vagas">
+                <MdSearch /> <p>Vagas</p>
                 </Link>
             </div>
-            <div className={styles.navBar_link}>
-                <Link to="/perfil">
-                <MdPerson /> Perfil
+            <div className={styles.navBar_links}>
+                <Link className={styles.navBar_link} to="/perfil">
+                <MdPerson /> <p>Perfil</p>
                 </Link>
             </div>
         </div>
+        
     )
 }
 
