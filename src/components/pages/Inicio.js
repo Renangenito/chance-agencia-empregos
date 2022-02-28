be469644be3a6d435324f26c2570667de78c1d8b
Renan Bezerra de Lima <77756047+Renangenito@users.noreply.github.com>
2022-02-28 18:42:15 -0300

    Atualizando o projeto!

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import BarraPesquisa from '../layout/BarraPesquisa';
-function Inicio(){
-    return(
-        <BarraPesquisa pagina="Início"/>
+import CardVaga from '../layout/CardVaga';
+import logoReact from '../../img/logo-react.png'
+
+import styles from './Inicio.module.css'
+function Inicio() {
+    return (
+        <div className={styles.inicio}>
+            <BarraPesquisa pagina='Início' />
+            <div className={styles.inicio_main}>
+                <h1>Bem vindo a Chance. Aqui você encontra diversas vagas de emprego</h1>
+                <p>Últimas vagas candidatas</p>
+            </div>
+            <div className={styles.vagas_main_cards}>
+            <CardVaga vaga='Desenvolvedor Front-end' alt='react' logo={logoReact}
+                    cidade='Suzano' estado='São Paulo' tipo='Remoto' contrato='CLT'  candidatura='Candidatura Confirmada' quando='há 3 dias'/>
+        </div>
+        </div>
     )
 }
 
--- src/components/pages/Vagas.js
+++ src/components/pages/Vagas.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import BarraPesquisa from '../layout/BarraPesquisa';
+import styles from './Vagas.module.css';
+import logoReact from '../../img/logo-react.png'
+import logoAngular from '../../img/logo-angular.png'
+import logoVue from '../../img/logo-vue.png'
+import logoNode from '../../img/logo-node.png'
+import CardVaga from '../layout/CardVaga';
 
-function Vagas(){
-    return(
-        <div>
-            <h1>Página Vagas</h1>
+function Vagas() {
+    return (
+        <div className={styles.vagas}>
+            <BarraPesquisa pagina='Vagas' />
+            <div className={styles.vagas_main}>
+                <CardVaga vaga='Desenvolvedor Front-end' alt='react' logo={logoReact}
+                    cidade='Suzano' estado='São Paulo' tipo='Remoto' contrato='CLT' quando='há 3 dias' />
+
+                <CardVaga vaga='Desenvolvedor Angular' alt='angular' logo={logoAngular}
+                    cidade='Mogi das Cruzes' estado='São Paulo' tipo='Presencial' contrato='CLT' quando='há 3 dias' />
+
+                <CardVaga vaga='Desenvolvedor Back-end' alt='node' logo={logoNode}
+                    cidade='Salvador' estado='Bahia' tipo='Remoto' contrato='PJ' quando='há 2 dias' />
+
+                <CardVaga vaga='Desenvolvedor Front-end' alt='vue' logo={logoVue}
+                    cidade='São Paulo' estado='São Paulo' tipo='Híbrido' contrato='CLT/PJ' quando='há 5 dias' />
+                <CardVaga vaga='Desenvolvedor Angular' alt='angular' logo={logoAngular}
+                    cidade='Mogi das Cruzes' estado='São Paulo' tipo='Presencial' contrato='CLT' quando='há 1 semana' />
+
+                <CardVaga vaga='Desenvolvedor Back-end' alt='node' logo={logoNode}
+                    cidade='Salvador' estado='Bahia' tipo='Remoto' contrato='PJ' quando='há 10h' />
+
+                <CardVaga vaga='Desenvolvedor Front-end' alt='vue' logo={logoVue}
+                    cidade='São Paulo' estado='São Paulo' tipo='Híbrido' contrato='CLT/PJ' quando='há 8 dias' />
+
+                <CardVaga vaga='Desenvolvedor Front-end' alt='react' logo={logoReact}
+                    cidade='Suzano' estado='São Paulo' tipo='Remoto' contrato='CLT' quando='há 1 dia' />
+
+                <CardVaga vaga='Desenvolvedor Back-end' alt='node' logo={logoNode}
+                    cidade='Salvador' estado='Bahia' tipo='Remoto' contrato='PJ' quando='há 2 dias' />
+
+
+
+            </div>
         </div>
     )
 }
